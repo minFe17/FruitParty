cd9b4cd795898dee9a8e8853d562a5ebb0c73f1b
minFe17 <vvcx1234@naver.com>
2023-09-16 21:36:50 +0900

    EventPanel 수정 Pade 애니메이션 Idle, Prefade 삭제

--- Assets/Scripts/UI/Event/EventPanel.cs
+++ Assets/Scripts/UI/Event/EventPanel.cs
@@ -6,4 +6,18 @@ public class EventPanel : MonoBehaviour
     [SerializeField] EventUIBase _shuffle;
     Dictionary<string, EventUIBase> _eventUI = new Dictionary<string, EventUIBase>();
 
+    public Dictionary<string, EventUIBase> EventUI
+    {
+        get
+        {
+            if (_eventUI.Count == 0)
+                AddEventUI();
+            return _eventUI;
+        }
+    }
+
+    void AddEventUI()
+    {
+
+    }
 }
--- Assets/Scripts/UI/Event/EventUIBase.cs
+++ Assets/Scripts/UI/Event/EventUIBase.cs
@@ -1,16 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class EventUIBase : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    
 }
